2143c9e5a0f27f379c0f7d4123c4854733d68dca
Wilson <wilsonthebaptiste@gmail.com>
2024-05-03 10:55:28 -0400

    contains the function for changing the active class on the navbar

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // this is for scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
 window.onscroll = () => {
+sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop -100
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+        // This will activate navbar links
+        navLinks.forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        })
+    }
+})
+
     //for a sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
-    
+
 }
